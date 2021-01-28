19a37307f828b504e1832228705dbaf75dc4ff06
kebukeyi <193276660@qq.com>
2021-01-28 23:37:43 +0800

    实践分布式事务实现方案以及思想；

---
+++ dtx-txmsg-demo/dtx-txmsg-demo-bank1/src/main/java/cn/itcast/dtx/txmsgdemo/bank1/service/AccountInfoService.java
@@ -0,0 +1,16 @@
+package cn.itcast.dtx.txmsgdemo.bank1.service;
+
+import cn.itcast.dtx.txmsgdemo.bank1.model.AccountChangeEvent;
+
+/**
+ * Created by Administrator.
+ */
+public interface AccountInfoService {
+
+    //向mq发送转账消息
+    void sendUpdateAccountBalance(AccountChangeEvent accountChangeEvent);
+
+    //更新账户，扣减金额
+    void doUpdateAccountBalance(AccountChangeEvent accountChangeEvent);
+
+}
